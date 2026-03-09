78912d7f965af83a649252b7852c12487ac253d9
Joakim Tviberg Fengås <117069660+jotvibfeng@users.noreply.github.com>
2026-03-09 15:14:29 +0100

    Created a navbar tsx file for cleaner code

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AutocomplettSearchBar from "./interface/AutocomplettSearchBar";
+import { Navbar } from "./components/navbar";
 
 interface Product {
   id: string;
@@ -23,8 +24,6 @@ interface Product {
 }
 
 function App() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   const [products, setProducts] = useState<Array<Product>>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
@@ -51,59 +50,12 @@ function App() {
       });
   }, []);
 
-  const toogleMenu = (): void => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   const purchaseButtonClick = (productId: string): void => {
     console.log(`Purchase button clicked for product ID: ${productId}`);
   };
   return (
     <>
-      <header className="text-white bg-[#7f7d7d30] ">
-        <nav className="flex items-center justify-between px-12 h-16 ">
-          <a href="#" className="text-2xl font-bold border-2 px-2 py-1">
-            OnlineShop
-          </a>
-
-          <div
-            className={`dropdown-menu absolute lg:static top-16 lg:top-0 left-0 lg:left-auto bg-[#7f7d7d30] lg:bg-transparent w-full lg:w-auto flex flex-col lg:flex-row gap-8 items-center py-2 lg:py-0 text-lg font-bold transition-all duration-300  ${
-              isMenuOpen
-                ? "translate-y-0 opacity-100"
-                : "-translate-y-full opacity-0 pointer-events-none lg:translate-y-0 lg:opacity-100 lg:pointer-events-auto"
-            }`}
-          >
-            <ul className="flex flex-col lg:flex-row gap-8">
-              <li className="hover:text-[#ffe9d0] cursor-pointer">
-                <Link to="/" onClick={() => setIsMenuOpen(false)}>
-                  Home
-                </Link>
-              </li>
-              <li className="hover:text-[#ffe9d0] cursor-pointer">
-                <Link to="/contact" onClick={() => setIsMenuOpen(false)}>
-                  Contact
-                </Link>
-              </li>
-            </ul>
-
-            <div className="flex flex-col lg:flex-row items-center gap-8">
-              <button className="cursor-pointer">
-                <i
-                  className="fa-solid fa-cart-arrow-down"
-                  style={{ color: "#7fd1ae" }}
-                ></i>
-              </button>
-            </div>
-          </div>
-          <div
-            className="toggle-button lg:hidden cursor-pointer"
-            onClick={toogleMenu}
-            aria-label="Toggle menu"
-          >
-            <i className="fa-solid fa-bars fa-lg"></i>
-          </div>
-        </nav>
-      </header>
+      <Navbar />
 
       <main>
         <AutocomplettSearchBar />
